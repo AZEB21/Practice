cf56bb2a1713f401d4dce826cb28a19a4456bfdc
Azeb Yirga <azyirga@gmail.com>
2026-05-03 20:53:20 +0600

    add crude an this

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.task_list, name='task-list'),
+    path('create/', views.create_task, name='create-task'),
+    path('update/<int:task_id>/', views.update_task, name='update-task'),
+   # path('delete/<int:task_id>/', views.delete_task, name='delete-task'),
+   path('github/', views.create_github),
+path('sport/', views.create_sport),
+path('music/', views.create_music),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,40 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from django.http import HttpResponse
 from .models import Task
+
+# CREATE
+def create_task(request):
+    Task.objects.create(title="learn github", completed=False)
+    return HttpResponse("Task created")
+def create_github(request):
+    Task.objects.create(title="learn github", completed=False)
+    return HttpResponse("GitHub task created")
+
+def create_sport(request):
+    Task.objects.create(title="learn sport", completed=False)
+    return HttpResponse("Sport task created")
+
+def create_music(request):
+    Task.objects.create(title="learn music", completed=False)
+    return HttpResponse("Music task created")
+
+# UPDATE
+def update_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.completed = True
+    task.save()
+    return HttpResponse("Task updated")
 
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+
+
+# DELETE
+# def delete_task(request, task_id):
+#     task = Task.objects.get(id=task_id)
+#     task.delete()
+#     return HttpResponse("Task deleted")
